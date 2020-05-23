08dac8dfeaac8b5c2e910a8f11b8eff7553adecd
Masum Islam <muhammed.islam@ericsson.com>
2020-05-23 15:06:25 +0300

    add aws java lambda demo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# aws-lambda-api-demo
+# AWS Lambda Java API Example
+
+This example code is used for running a Java Lambda using API Gateway trigger. 
+
+It uses AWS Lambda Core SDK for `RequestHandler` implementation which is called by the trigger
+
+## Architecture Used
+
+<img src="./architecture.png" width="100%">
---
+++ src/main/java/com/techprimers/aws/LambdaJavaAPI.java
@@ -0,0 +1,24 @@
+package com.techprimers.aws;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Collections;
+
+public class LambdaJavaAPI implements RequestHandler<Object, GatewayResponse> {
+
+    @Override
+    public GatewayResponse handleRequest(Object object, Context context) {
+
+        String message = "Hello from masum";
+        System.out.println(message);
+
+        GatewayResponse response = new GatewayResponse(
+                message,
+                200,
+                Collections.singletonMap("X-Powered-By", "masum islam"),
+                false
+        );
+        return response;
+    }
+}
